2166328fe16f42b6c45963f1685dfe9ecef92ec7
Chanwoo Kim <kcw2205@naver.com>
2024-07-01 17:35:08 +0900

    Update Polynesiaglot （Large）.cc

--- 백준/Silver/12039. Polynesiaglot （Large）/Polynesiaglot （Large）.cc
+++ 백준/Silver/12039. Polynesiaglot （Large）/Polynesiaglot （Large）.cc
@@ -1,3 +1,16 @@
+/**
+ * vowel : consist of minimum 1 character.
+ * consonant : consist of minimum 2 characters.
+ * so this will be a combination problem, which consists of
+ * consonant form : vowel * consonant, where its length is 2.
+ * vowel form : vowel where its length is 1.
+ * if 1 1 4, then consonant : 1, vowel form
+ * 4 length character could be partitioned as `1 + 1 + 1 + 1`, `1 + 1 + 2`, `2 +
+ * 2` but this partitions should consider orders. which means, for example,
+ * `2+1+1` and `1+2+1` are different. this method could be implmented via
+ * dynamic programming easily, i guess.
+ */
+
 #include <iostream>
 using namespace std;
 
