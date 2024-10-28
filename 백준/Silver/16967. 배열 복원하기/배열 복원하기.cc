35b27bfdb2a8e8fed04c83b831dc2606e588ac6f
Chanwoo Kim <kcw2205@naver.com>
2024-10-29 00:48:20 +0900

    [Silver III] Title: 배열 복원하기, Time: 104 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/16967. 배열 복원하기/배열 복원하기.cc
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+
+int mat[1000][1000];
+
+int main() {
+    int h, w, x, y; cin>>h>>w>>x>>y;
+    for(int i=0; i<x + h; ++i) {
+        for(int j=0; j<y + w; ++j) {
+            cin>>mat[i][j];
+        }
+    }
+
+    for(int i=0; i<h; ++i) {
+        for(int j=0; j<w; ++j) {
+            if (i >= x && j >= y) {
+                cout<<mat[i][j] - mat[i-x][j-y]<<' ';
+                mat[i][j] = mat[i][j] - mat[i-x][j-y];
+            }
+            else {
+                cout<<mat[i][j]<<' ';
+            }
+        }
+        cout<<'\n';
+    }
+}
